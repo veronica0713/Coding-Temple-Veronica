bae2e2efb1c798c182f97d61d5d9c596b34f28d7
Anatoliy Kolodkin <anatoliy@ankosoftware.com>
2018-03-29 13:36:58 -0500

    Add project files.

---
+++ week2-10/Point.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week2_10
+{
+    class Point
+    {
+    //Even though you don't see it, every class has a "Default" constructor.
+    //Constructors "set up" your class for use.. 
+    //public Point()
+    //{
+
+
+    //}
+
+    //If you add parameters to the constructor 
+    //and don't explicitly also define a paramterless constructor,
+    //DotNet will not "generate" the parameterless constructor for you.
+    //This is an indication to other developers that they MUST supply those parameters
+    //in order to use your class.
+        public Point(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public double x { get; set; }
+        public double y { get; set; }
+      
+
+    }
+
+
+}
+
---
+++ week2-10/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week2_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Point a = new Point(0.0, 1.0);
+            Point b = new Point(0.0, 2.0);
+            var dis = Distance(a, b);
+
+         
+            Console.WriteLine("Distance: {0:N}", dis);
+
+            Console.ReadLine();
+
+        }
+
+        public static double Distance(Point p1, Point p2)
+
+        {           
+            return (Math.Pow(p1.x - p2.x, 2) + Math.Pow(p1.y - p2.y, 2));
+            
+        }
+    }
+}
